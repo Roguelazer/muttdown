0adef389c744924a8e4a118a7a29552c53c2102f
James Brown <roguelazer@roguelazer.com>
2018-10-22 09:10:31 -0700

    fix bug with smtp_password_command output encoding; add tests of config loader

---
+++ tests/test_config.py
@@ -0,0 +1,27 @@
+import tempfile
+
+from muttdown.config import Config
+
+
+def test_smtp_password_literal():
+    c = Config()
+    c.merge_config({'smtp_password': 'foo'})
+    assert c.smtp_password == 'foo'
+
+
+def test_smtp_password_command():
+    c = Config()
+    c.merge_config({'smtp_password_command': 'sh -c "echo foo"'})
+    assert c.smtp_password == 'foo'
+
+
+def test_css():
+    c = Config()
+    c.merge_config({'css_file': None})
+    assert c.css == ''
+
+    with tempfile.NamedTemporaryFile(delete=True) as css_file:
+        css_file.write(b'html { background-color: black; }\n')
+        css_file.flush()
+        c.merge_config({'css_file': css_file.name})
+        assert c.css == 'html { background-color: black; }\n'
